0d6eec2c4f2d20c678f9bb2fa43e197d65749f2d
DulakshiGuruge2001 <pramodhyadulakshi@gmail.com>
2025-07-28 16:26:05 +0530

    Update models.py
    add a new line

--- app/models.py
+++ app/models.py
@@ -2,7 +2,8 @@ from app import db
 
 class Menu(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True)
+    name = db.Column(db.String(64), index=True, nullable=False)
+    description = db.Column(db.String(128), nullable=True)  
 
-    def __repr__(self):
+    def _repr_(self):
         return '<Menu {}>'.format(self.name)
